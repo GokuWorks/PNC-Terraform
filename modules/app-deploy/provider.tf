e032f67947de3a6f33b358512d1affebf73a66fa
GokuWorks <sayon7511@gmail.com>
2025-05-30 02:59:33 +0000

    Add traefik

---
+++ modules/app-deploy/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+      helm = {
+        source  = "hashicorp/helm"
+        version = "3.0.0-pre2"
+      }
+      kubectl = {
+        source  = "gavinbunney/kubectl"
+        version = ">= 1.7.0"
+      }
+    }
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = var.kubeconfig_path
+  }
+}
+
+provider "kubectl" {
+  config_path = var.kubeconfig_path
+}
