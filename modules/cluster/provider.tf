cbbf5f5783bddadfcf8f0700670f40a4f5fc9d40
GokuWorks <sayon7511@gmail.com>
2025-09-20 19:42:07 +0000

    Remove verson req from cluster module

--- modules/cluster/provider.tf
+++ modules/cluster/provider.tf
@@ -2,15 +2,12 @@ terraform {
     required_providers {
       proxmox = {
         source = "bpg/proxmox"
-        version = "0.77.1"
       }
       null = {
         source  = "hashicorp/null"
-        version = "~> 3.2"
       }
       local = {
         source  = "hashicorp/local"
-        version = "~> 2.5"
       }
     }
 }
